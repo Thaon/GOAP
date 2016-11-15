36d43264423329eff75e02a264b63e4a91052a72
YAP! <gabriele.maddaloni@gmail.com>
2016-11-15 21:57:57 +0000

    outlining the base system

---
+++ Assets/Code/GOAPPlanner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GOAPPlanner : MonoBehaviour {
+
+    #region member variables
+
+    private List<Precondition> m_preconditions;
+    private List<Effect> m_results;
+    private Stack<Action> m_goapPlan;
+
+    #endregion
+
+    void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	
+	}
+
+    public void Plan()
+    {
+
+    }
+}
---
+++ Assets/Code/GoapAgent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GoapAgent : MonoBehaviour {
+
+    #region member variables
+
+    private List<Precondition> m_preconditions;
+    private List<Effect> m_results;
+
+    #endregion
+
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
